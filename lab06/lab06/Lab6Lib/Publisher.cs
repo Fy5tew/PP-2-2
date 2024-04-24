2f1d4e712393197f98a954a18e8de1d380370ae4
Nikita <nikturchinovich@mail.ru>
2024-04-24 17:09:10 +0300

    lab06 is done

---
+++ lab06/lab06/Lab6Lib/Publisher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6Lib {
+    public class Publisher {
+        private delegate void SubscriberUpdate(string eventname);
+        private event SubscriberUpdate? _update;
+        private string _eventname;
+
+        public Publisher(string eventname) {
+            _eventname = eventname;
+        }
+
+        public void subscribe(ISubscriber subscriber) {
+            _update += subscriber.update;
+        }
+
+        public bool unsubscribe(ISubscriber subscriber) {
+            _update -= subscriber.update;
+            return true;
+        }
+
+        public int nonify() {
+            _update?.Invoke(_eventname);
+            return _update?.GetInvocationList()?.Length ?? 0;
+        }
+    }
+}
